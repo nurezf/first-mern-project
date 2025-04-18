c4db2451d7874d90d96de88e5c6e20f269086979
nurezf <nuredinzf1996@gmail.com>
2025-04-19 00:25:07 +0300

    update methode buited

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from"dotenv";
 import { connectDB } from "./config/db.js";
 import Product from "./models/product.model.js";
+import mongoose from "mongoose";
 
 
 dotenv.config();
@@ -38,6 +39,23 @@ try{
     console.error("error in creating product",error.message)
 }
 
+})
+
+app.put("/api/products/:id",async (req,res)=>{
+    const {id}=req.params;
+
+    const product=req.body;
+
+    if(!mongoose.Types.ObjectId.isValid(id)){
+      return  res.status(404).json({sucess:false,message:"invalid product id"});
+    }
+
+    try{
+       const updateProduct= await Product.findByIdAndUpdate(id,product,{new:true});
+       res.status(200).json({success:true,data:updateProduct})
+    }catch(error){
+        res.status(500).json({success:false,message:"error updating product"});
+    }
 })
 
 app.delete("/api/products/:id",async (req,res)=>{
