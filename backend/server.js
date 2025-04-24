948c13a5c2897de0aacc9a94e7ceeb9b99c1124d
nurezf <nuredinzf1996@gmail.com>
2025-04-24 21:27:32 +0300

    set uo for deployment

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import path from "path";
 import { connectDB } from "./config/db.js";
 
 import productRoutes from "./routes/product.route.js";
@@ -10,9 +11,22 @@ const app = express();
 
 app.use(express.json());
 
+const __dirname = path.resolve();
+
 const port = process.env.PORT;
 
 app.use("/api/products", productRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+  });
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running...");
+  });
+}
 
 app.listen(port, () => {
   connectDB();
